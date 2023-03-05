556d58a1ea356994eb4e40c1e0e4207fd60a1a2d
Peter Bugala <peter.bugala21@gmail.com>
2023-03-05 14:20:34 +0100

    Remove global init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueQrcodeReader from "vue-qrcode-reader";
-Vue.use(VueQrcodeReader);
-Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
